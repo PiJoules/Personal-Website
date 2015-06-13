5b182629422006a3ec8922d045b6533b93c45614
pijoules <lchan1994@yahoo.com>
2015-06-13 17:47:22 -0400

    moved stuff

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import json
 
-# Import the Flask Framework
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+with open('posts.json') as data_file:    
+    posts = json.load(data_file)
 
-@app.route('/')
-def hello():
+@app.route("/")
+@app.route("/posts/")
+def index():
     return render_template("index.html",
     	page_count=1,
-    	page_num=0
+    	page_num=0,
+    	posts=posts
     )
 
+
+@app.route("/posts/<num>/")
+def post(num):
+	if int(num) < len(posts):
+		return render_template("posts/" + num + ".html")
+	else:
+		abort(404)
 
 @app.errorhandler(404)
 def page_not_found(e):
