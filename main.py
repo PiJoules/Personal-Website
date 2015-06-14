131bfe64f134288c84af8424bd75f8d5c2564035
pijoules <lchan1994@yahoo.com>
2015-06-13 21:27:31 -0400

    aded pagination

--- main.py
+++ main.py
@@ -14,13 +14,24 @@ with open('posts.json') as data_file:
 # Essentially filters the posts based on the search query
 def search_posts(query):
 	matches = []
-	for i in range(len(posts)):
-		content = render_template("posts/" + str(i) + ".html")
+	for post in posts:
+		content = render_template("posts/" + post["file"])
 		count = content.count(query)
 		if count > 0:
-			matches.append( (posts[i], count) )
+			matches.append( (post, count) )
 	sorted(matches, key=lambda match: match[1], reverse=True)
 	return [m[0] for m in matches]
+
+def get_adj_posts(post_file):
+	adj_posts = {}
+	for i in range(len(posts)):
+		if posts[i]["file"] == post_file:
+			if i > 0:
+				adj_posts["prev"] = posts[i-1]
+			if i < len(posts)-1:
+				adj_posts["next"] = posts[i+1]
+			break
+	return adj_posts
 
 @app.route("/")
 @app.route("/post/")
@@ -39,11 +50,11 @@ def about():
 def contact():
 	return render_template("contact.html", page_count=1, page_num=0)
 
-@app.route("/post/<num>/")
-def post(num):
-	if int(num) < len(posts):
-		return render_template("posts/" + num + ".html")
-	else:
+@app.route("/post/<path>")
+def post(path):
+	try:
+		return render_template("posts/" + path, adj_posts=get_adj_posts(path))
+	except:
 		abort(404)
 
 @app.errorhandler(404)
