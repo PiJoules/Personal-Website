f6974e4f217278626a58704da14c6afe094b01bf
Leo is awesome <lchan1994@yahoo.com>
2015-07-08 14:46:18 -0400

    gotta increase that github score

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Leo's Page</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet"/>
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="top-row" class="container">
+			<img id="name" src="images/leonardchan.png" alt="Leonard Chan"/>
+		</div>
+
+		<div class="container margin-left-right-0 width-100">
+			<div class="row">
+				<div class="col-md col-md-4 padding-left-right-0">
+					<div class="img-container2">
+						<img style="width: 100%; height: 600px;" src="images/somephoto.jpg"/>
+					</div>
+				</div>
+				<div id="intro1" class="col-md-4 intro forum-content">
+					<h1 id="hello">Hello,</h1>
+					<h1>My name Leo and this is my website.</h1>
+				</div>
+				<div class="col-md col-md-4 padding-left-right-0">
+					<img style="width: 100%; height: 100%;" src="images/red.jpg"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md col-md-8 col-md-offset-4 padding-left-right-0">
+					<div class="img-container">
+						<img style="width: 100%;" src="images/sentry.jpg"/>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="intro2" class="col-md-3 intro">
+					<h1>I enjoy playing a game called.</h1>
+				</div>
+				<div class="col-md-8 padding-left-right-0">
+					<div class="img-container">
+						<img class="width-100 margin-top-m50px" src="images/soldier.jpg"/>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+
+		<script type="text/javascript">
+			window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+			    alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
+			    + ' Column: ' + column + ' StackTrace: ' +  errorObj);
+			}
+		</script>
+	</body>
+</html>
